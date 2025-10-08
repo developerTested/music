486b493a826fa4f6e589e652079d9ef979294fa3
Developer Tested <89337605+developerTested@users.noreply.github.com>
2025-10-08 23:10:37 +0530

    add tanstack query

--- src/main.tsx
+++ src/main.tsx
@@ -4,17 +4,22 @@ import RouteList from './route'
 import { Provider as ReduxProvider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
 import { ToastContainer } from 'react-toastify'
-import { store, persistedStore } from './redux/store'
+import { persistedStore, store } from './redux/store'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 import "./styles/global.css"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ReduxProvider store={store}>
-      <PersistGate persistor={persistedStore} loading={null}>
-        <RouteList />
-      </PersistGate>
-    </ReduxProvider>
+    <QueryClientProvider client={queryClient}>
+      <ReduxProvider store={store}>
+        <PersistGate persistor={persistedStore} loading={null}>
+          <RouteList />
+        </PersistGate>
+      </ReduxProvider>
+    </QueryClientProvider>
     <ToastContainer position="top-center" theme="colored" />
   </StrictMode>,
 )
